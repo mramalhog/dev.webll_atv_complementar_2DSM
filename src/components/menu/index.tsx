b365902d8aa0a0c80e1f039fce810c3a7f1d89ab
mramalhog <marirg28@gmail.com>
2023-11-06 21:33:30 -0300

    primeiro

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,14 +6,14 @@ export default function Menu(){
     const {ativo,setAtivo} = useLoteria();
     return(
         <div>
-            <Link to="/megasena" className={ativo === "megasena" ? "ativo" : "megasena"} onClick={() => setAtivo("megasena")}>
-                Megasena
+            <Link to="/megasena" className={ativo === "megasena" ? "botao ativo" : "botao megasena"} onClick={() => setAtivo("megasena")}>
+                MEGASENA
             </Link>
-            <Link to="/quina" className={ativo === "quina" ? "ativo" : "quina"} onClick={() => setAtivo("quina")}>
-                Quina
+            <Link to="/quina" className={ativo === "quina" ? "botao ativo" : "botao quina"} onClick={() => setAtivo("quina")}>
+                QUINA
             </Link>
-            <Link to="/lotofacil" className={ativo === "lotofacil" ? "ativo" : "lotofacil"} onClick={() => setAtivo("lotofacil")}>
-                Lotofácil
+            <Link to="/lotofacil" className={ativo === "lotofacil" ? "botao ativo" : "botao lotofacil"} onClick={() => setAtivo("lotofacil")}>
+                LOTOFÁCIL
             </Link>
         </div>
     )
